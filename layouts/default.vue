4c54aa8975398b91b63e1093e351261414aad41e
Jorge Arguelles <jorge.arguelles@outlook.com>
2023-05-11 14:11:33 -0600

    Configuracion Inicial de Nuxtjs

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<TheMenu />
+			<Nuxt />
+		<TheFooter />
+	</div>
+</template>
+
+
+<script>
+	import TheMenu from '~/components/TheMenu.vue'
+	import TheFooter from '~/components/TheFooter.vue'
+
+	export default {
+		name: 'default',
+		components: {
+			TheMenu,
+			TheFooter
+		}
+	}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		Inicio
+	</div>
+</template>
+
+
+<style>
+</style>
+
+
+<script>
+	export default {
+		name: 'inicio',
+		head() {
+			return {
+				title: 'Acuantia | Inicio'
+			}
+		}
+	}
+</script>
